3002e007b908b411a61cf8f80f3fa07d29ab2379
bulklc <clog@ucdavis.edu>
2021-05-18 17:55:47 -0700

    modified:   js/menu_list.js

--- js/menu_list.js
+++ js/menu_list.js
@@ -9,7 +9,7 @@ var menu_list = [
         "Creams": ["None","1","2","3"],
         "Sugars": ["None","1","2","3"],
         "Type": ["Hot","Iced"],
-        "image_url": "images/coffee.png",
+        "image_url": "images/coffee.png"
     },
 
     {
@@ -21,7 +21,7 @@ var menu_list = [
         "Creams": ["None","1","2","3"],
         "Sugars": ["None","1","2","3"],
         "Type": ["Hot","Iced"],
-        "image_url": "images/americano.png",
+        "image_url": "images/americano.png"
     },
 
     {
@@ -33,7 +33,7 @@ var menu_list = [
         "Milk": ["Whole","LowFat","Nonfat","Almond"],
         "Sugars": ["None","1","2","3"],
         "Type": ["Hot","Iced","Blended"],
-        "image_url": "images/latte.png",
+        "image_url": "images/latte.png"
     },
 
     {
@@ -45,7 +45,7 @@ var menu_list = [
         "Milk": ["Whole","LowFat","Nonfat","Almond"],
         "Sugars": ["0","1","2","3"],
         "Type": ["Hot","Iced"],
-        "image_url": "images/cappuccino.png",
+        "image_url": "images/cappuccino.png"
     },
 
     {
@@ -57,7 +57,7 @@ var menu_list = [
         "Milk": ["Whole","LowFat","Nonfat","Almond"],
         "Type": ["Hot","Iced","Blended"],
         "Whipped_Cream": ["Yes","No"],
-        "image_url": "images/mocha.png",
+        "image_url": "images/mocha.png"
     },
 
     {
@@ -69,8 +69,7 @@ var menu_list = [
         "Spread": ["Butter","Cream Cheese","Peanut Butter"],
         "Jam": ["Strawberry","Grape","Peach"],
         "Toasted": ["Yes", "No"],
-
-        "image_url": "images/bagels.png",
+        "image_url": "images/bagels.png"
     },
 
     {
@@ -80,7 +79,7 @@ var menu_list = [
         "description": "Our classic croissant is made with real butter to create a golden, crunchy top with soft, flaky layers inside. ",
         "Price": ["Plain - $2.75","Choclate - $3.00","Almond - $3.25"],
         "Toasted": ["Yes","No"],
-        "image_url": "images/crousants.png",
+        "image_url": "images/crousants.png"
     },
 
     {
@@ -90,7 +89,7 @@ var menu_list = [
         "description": "Our delicious muffins are both sweet and moist whether you enjoy chocolate chip, blueberry , or pumkin",
         "Price": ["Choclate Chip - $2.25","Blueberry - $2.50","Pumpkin - $2.75"],
         "Heated": ["Yes","No"],
-        "image_url": "images/muffins.png",
+        "image_url": "images/muffins.png"
     },
 
     {
@@ -100,7 +99,7 @@ var menu_list = [
         "description": "A traditional scone with chocolate chips, blueberries, or pumpkin is a wholesome treat.",
         "Price": ["Choclate Chip - $2.75","Blueberry - $3.00","Pumpkin - $3.25"],
         "Heated": ["Yes","No"],
-        "image_url": "images/scones.png",
+        "image_url": "images/scones.png"
     },
     
     
@@ -111,7 +110,7 @@ var menu_list = [
         "description": "Our Famous T-Shirt, ethically-sourced, 100% Cotton.",
         "Price": ["Standard - $17.00", "Collector's - $20.00"],
         "Type": ["Small","Medium","Large","Extra Large"],
-        "image_url": "images/t-shirt.png",
+        "image_url": "images/t-shirt.png"
     },
     
     {
@@ -120,7 +119,7 @@ var menu_list = [
         "category":"merch", 
         "description": "For Family, Friends, and all lovers of Javalicious",
         "Price": ["$5.00","$10.00","$20.00","$50.00"],
-        "image_url": "images/gift-card.png",
+        "image_url": "images/gift-card.png"
     },
     
         {
@@ -131,8 +130,8 @@ var menu_list = [
         "Price": ["Small - $5.00","Medium - $10.00","Large - $15.00"],
         "Milk": ["None","2%","Whole","Almond","Soy"],
         "Fruit": ["Blueberries","Strawberries","Banana"],
-        "image_url": "images/granola.jpg",
-    //}  
+        "image_url": "images/granola.jpg"
+        } 
 
 ];
 
